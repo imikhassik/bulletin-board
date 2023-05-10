ac277ed22445de9fb35c4cb53887f3fbcfb6061f
Ilya Mikhasik <imikhassik@gmail.com>
2023-05-10 18:38:09 +0300

    Add models

--- bulletin_board/board/models.py
+++ bulletin_board/board/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=254)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+class Attachment(models.Model):
+    name = models.CharField(max_length=254)
+    file = models.FileField(upload_to='attachments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    CATEGORY_CHOICES = [
+        ('Tanks', 'Танки'),
+        ('Healers', 'Хилы'),
+        ('DD', 'ДД'),
+        ('Guildmasters', 'Гилдмастеры'),
+        ('Questgivers', 'Квестгиверы'),
+        ('Smiths', 'Кузнецы'),
+        ('Tanners', 'Кожевники'),
+        ('Potionbrewers', 'Зельевары'),
+        ('Spellmasters', 'Мастера заклинаний')
+    ]
+    name = models.CharField(max_length=254, choices=CATEGORY_CHOICES)
+
+
+class Response(models.Model):
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- bulletin_board/bulletin_board/settings.py
+++ bulletin_board/bulletin_board/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
